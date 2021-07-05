a4a2447c99394cf3031e600aa7be9b75090933e2
LuizF12 <luizfelip079@outlook.com>
2021-07-05 21:38:20 -0200

    piano

---
+++ script.js
@@ -0,0 +1,143 @@
+//primeiro audio
+
+var C = new Audio();
+C.src = 'notas/notes_C.mp3';
+
+//segundo audio
+
+var Db = new Audio();
+Db.src = 'notas/notes_Db.mp3';
+
+//terceiro audio
+
+var D = new Audio();
+D.src = 'notas/notes_D.mp3';
+
+//quarto audio
+
+var Eb = new Audio();
+Eb.src = 'notas/notes_Eb.mp3';
+
+//quinto audio
+
+var E = new Audio();
+E.src = 'notas/notes_E.mp3';
+
+//sexto audio
+
+var F = new Audio();
+F.src = 'notas/notes_F.mp3';
+
+//setimo audio
+
+var Gb = new Audio();
+Gb.src = 'notas/notes_Gb.mp3';
+
+//oitavo audio
+
+var G = new Audio();
+G.src = 'notas/notes_G.mp3';
+
+//nono audio
+
+var Ab = new Audio();
+Ab.src = 'notas/notes_Ab.mp3';
+
+//decimo audio
+
+var A = new Audio();
+A.src = 'notas/notes_A.mp3';
+
+//decimo primeiro audio
+
+var Bb = new Audio();
+Bb.src = 'notas/notes_Bb.mp3';
+
+//ultimo audio
+
+var B = new Audio();
+B.src = 'notas/notes_B.mp3';
+
+//tocar o primeiro audio
+
+function playC(){
+				C.play();
+				C.currentTime = 0;
+}
+
+//tocar segundo audio
+
+function playDb(){
+				Db.play();
+				Db.currentTime = 0;
+}
+
+//tocar terceiro audio
+
+function playD(){
+				D.play();
+				D.currentTime = 0;
+}
+
+//tocar quarto audio
+
+function playEb() {
+				Eb.play();
+				Eb.currentTime = 0;
+}
+
+//tocar quinto audio
+
+function playE(){
+				E.play();
+				E.currentTime = 0;
+}
+
+//tocar sexto audio
+
+function playF() {
+				F.play();
+				F.currentTime = 0;
+}
+
+//tocar setimo audio
+
+function playGb() {
+				Gb.play();
+				Gb.currentTime = 0;
+}
+
+//tocar oitavo audio
+
+function playG() {
+				G.play();
+				G.currentTime = 0;
+}
+
+//tocar nono audio 
+
+function playAb(){
+				Ab.play();
+				Ab.currentTime = 0;
+}
+
+//tocar decimo audio
+
+function playA(){
+				A.play();
+				A.currentTime = 0
+}
+
+//tocar decimo primeiro audio
+
+function playBb(){
+				Bb.play();
+				Bb.currentTime = 0;
+}
+
+//tocar ultimo audio
+
+function playB(){
+				B.play();
+				B.currentTime = 0;
+}
